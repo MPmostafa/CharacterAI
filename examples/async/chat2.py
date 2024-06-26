0e4bb8d3db7965245903f0a2ae7e3aa713c64a79
MPmostafa <grfaf@icloud.com>
2024-06-26 17:08:02 +0300

    Update chat2.py

--- examples/async/chat2.py
+++ examples/async/chat2.py
@@ -2,16 +2,12 @@ from characterai import aiocai
 import asyncio
 
 async def main():
-    char = input('CHAR ID: ')
-
-    client = aiocai.Client('TOKEN')
-
+    char = "De_AzlU9pHh8ujlZ672Va2H4esHlGrAnVlgzJQzivAw"
+    client = aiocai.Client('b2daeded189d8ea3cadfcae9aa159d90966775fd')
+    chat = 'f89c3b7d-9fd9-420c-a98a-a3916a06b34'
     me = await client.get_me()
 
     async with await client.connect() as chat:
-        new, answer = await chat.new_chat(
-            char, me.id
-        )
 
         print(f'{answer.name}: {answer.text}')
         
@@ -19,7 +15,7 @@ async def main():
             text = input('YOU: ')
 
             message = await chat.send_message(
-                char, new.chat_id, text
+                char, chat, text
             )
 
             print(f'{message.name}: {message.text}')
